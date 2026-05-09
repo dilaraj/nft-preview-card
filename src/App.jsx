b365f1657aff35839bacf775a96b5cc6ea9e2ab4
dilaraj <dilara.ajaj369@gmail.com>
2026-05-09 20:02:10 +1000

    finalising project code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import './App.css'
+import NFTIcon from './assets/images/image-equilibrium.jpg'
+import ETHIcon from './assets/images/icon-ethereum.svg'
+import ClockIcon from './assets/images/icon-clock.svg'
+import Avatar from './assets/images/image-avatar.png'
 
 function App() {
 
   return (
-    <div>Hello World</div>
+    <div className="main-component-container">
+      <div className="nft-img-container">
+        <img src={NFTIcon} alt="Equilibrium NFT Icon" className='nft-img'/>
+      </div>
+      <div className="nft-description-container">
+        <div className="nft-name">Equilibrium #3429</div>
+        <div className="nft-description">Our Equilibrium collection promotes balance and calm.</div>
+        <div className="eth-info">
+          <div className="eth-price">
+            <img src={ETHIcon} alt="eth icon"/>
+            <span>0.041 ETH</span>
+          </div>
+          <div className="eth-date">
+            <img src={ClockIcon} alt="Clock Icon" />
+            <span>3 days left</span>
+          </div>
+        </div>
+        <div className="line"></div>
+        <div className="nft-creator">
+          <div className="creator-avatar">
+            <img src={Avatar} alt="Avatar icon" className='avatar-icon'/>
+          </div>
+          <div className="nft-creator-info">
+            Creation of <span>Jules Wyvern</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
